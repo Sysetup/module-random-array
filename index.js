aec2ab4fdb1a1be1dccc91e50b17cc579d490820
x <x@x.x>
2022-12-02 13:04:43 -0500

    Code comments added.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/**
+   ____             __             
+  / __/_ _____ ___ / /___ _____ _/|
+ _\ \/ // (_-</ -_) __/ // / _ > _<
+/___/\_, /___/\__/\__/\_,_/ .__//  
+    /___/                /_/       
+> Systems development company.
+
+*/
+
+/**
+ * 
+ * @function randomArray This script shuffles the elements of an array.
+ * @param {Array} arr 
+ */
+
 const randomArray = (arr) => {
     arr.sort(() => Math.random() - 0.5)
 } 
